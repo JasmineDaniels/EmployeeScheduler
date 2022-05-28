a4194c559852ea1c4bc1a7b9093cc5a098b0340a
Jasmine Daniels <create.jasminedaniels@gmail.com>
2022-05-28 13:28:31 -0400

    assigned days to Calendar --js

---
+++ assets/script.js
@@ -0,0 +1,34 @@
+var calendar = document.querySelector(".app-calendar")
+// console.log(calendar)
+
+//Function to Highlight Weekends
+var isWeekend = day => {
+    // 0 when it's Sunday, 6 when its Saturday
+   return day % 7 === 6 || day % 7 === 0
+} 
+
+// Function to get the name of Days (Sun - Sat)
+var getDayName = day => { 
+    var date = new Date(Date.UTC(2022, 7, day)); // UTC Time zone YEAR, MONTH[i](June), DAYS -- date.now will give you todays date 
+    // short = short format 
+    var dayName = new Intl.DateTimeFormat("en-US", { weekday: "short" }).format(date);
+    return dayName
+}
+
+for (let day = 1; day <= 31; day++){
+    // console.log(day)
+    const weekend = isWeekend(day);
+
+    var insertName = " ";
+    if (day <= 7){
+        var nameDays = getDayName(day);
+        insertName = `<div class= "names">${nameDays}</div>`;
+    }
+    calendar.insertAdjacentHTML("beforeend", `<div 
+    class= "day ${weekend ? "weekend" : ""}">${insertName}${day}</div>`);
+    
+}
+
+
+
+
